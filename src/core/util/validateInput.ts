17cb8167e307a92ecf596a039f3b81d7d15fa712
pranavraagz <pranavraagz@gmail.com>
2022-06-01 16:06:23 +0530

    fix: improved input validation using regex

--- src/core/util/validateInput.ts
+++ src/core/util/validateInput.ts
@@ -2,7 +2,12 @@ import pkg from "semver";
 const { valid } = pkg;
 
 export function validateInput(s: string): boolean {
-  // Validate input semver
+  // Evaluate basic structure of input <string>@<string>
+  let regex = /^(.+)@(.+)$/i;
+  if (regex.test(s) == false) {
+    return false;
+  }
+  // Validate version number using semver
   const vNumber = s.split("@")[1];
   // valid() returns a string if input is valid version number
   if (typeof valid(vNumber) != "string") {
